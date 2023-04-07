67a159745253e4e265f6a7ead36212963963333a
Dominic <lovric.domagoj1@gmail.com>
2023-04-07 18:36:41 +0100

    Fully working version of Audio Chat GPT

---
+++ audio_gpt/elevenlabs_voices.py
@@ -0,0 +1,51 @@
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# voice settings
+STABILITY = 0
+SIMILARITY_BOOST = 0
+
+# streaming chunk size
+CHUNK_SIZE = 1024
+
+XI_API_KEY = os.getenv("ELEVEN_API_KEY")
+OUTPUT_PATH = os.path.abspath("./output_voice.mp3")
+
+headers = {
+  "Accept": "application/json",
+  "xi-api-key": XI_API_KEY,
+}
+
+
+data = {
+  "text": "Some very long text to be read by the voice",
+  "voice_settings": {
+    "stability": STABILITY,
+    "similarity_boost": SIMILARITY_BOOST
+  }
+}
+
+voice_id = "21m00Tcm4TlvDq8ikWAM"
+tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
+
+response = requests.post(tts_url, json=data, headers=headers, stream=True)
+
+with open(OUTPUT_PATH, 'wb') as f:
+    for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+        if chunk:
+            f.write(chunk)
+
+# Retrieve history. It should contain generated sample.
+history_url = "https://api.elevenlabs.io/v1/history"
+
+headers = {
+  "Accept": "application/json",
+  "xi-api-key": XI_API_KEY
+}
+
+response = requests.get(history_url, headers=headers)
+
+print(response.text)
--- audio_gpt/web.py
+++ audio_gpt/web.py
@@ -1,14 +1,35 @@
 import gradio as gr
 import openai
 import os
-import subprocess
+import requests
 from dotenv import load_dotenv
 import soundfile as sf
 import pyttsx3
+import subprocess
+
 
 load_dotenv()
 openai.api_key = os.getenv("API_KEY")
+XI_API_KEY = os.getenv("ELEVEN_API_KEY") # ElevenLabs
 
+# voice settings
+STABILITY = 0
+SIMILARITY_BOOST = 0
+
+# streaming chunk size
+CHUNK_SIZE = 1024
+OUTPUT_PATH = os.path.abspath("./output_voice.mp3")
+voice_id = "21m00Tcm4TlvDq8ikWAM"  # American Female
+
+
+# Headers for ElevenLabs (Voice output)
+headers = {
+    "Accept": "application/json",
+    "xi-api-key": XI_API_KEY,
+    "Content-Type": "application/json"
+}
+
+# ChatGPT Prompt Engineering
 messages = [{"role": "system", "content": 'You are a therapist. Respond to all input in 25 words or less.'}]
 
 
@@ -30,12 +51,27 @@ def transcribe_message(audio):
     system_message = response["choices"][0]["message"]
     messages.append(system_message)
 
-    def say_message(message):
-        engine = pyttsx3.init()
-        engine.say(message)
-        engine.runAndWait()
+    data = {
+        "text": system_message["content"],
+        "voice_settings": {
+            "stability": STABILITY,
+            "similarity_boost": SIMILARITY_BOOST
+        }
+    }
 
-    say_message(system_message["content"])
+    voice_id = "21m00Tcm4TlvDq8ikWAM"
+    tts_url = f"https://api.elevenlabs.io/v1/text-to-speech/{voice_id}/stream"
+
+    response = requests.post(tts_url, json=data, headers=headers, stream=True)
+
+    with open(OUTPUT_PATH, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=CHUNK_SIZE):
+            if chunk:
+                f.write(chunk)
+
+    # Play the audio file
+    audio_path = r"G:\My Drive\Arc Capital\Python\openai_playground\audio_gpt\output_voice.mp3"
+    os.system(f'start "" "{audio_path}"')
 
     chat_transcript = ""
     for message in messages:
